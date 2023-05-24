702bd1087590e7238df9cb617ae0c2a3a894e8a0
dmaljovec <daniel.maljovec@recursionpharma.com>
2023-05-24 11:44:51 -0600

    chore: defer semantic versioning to setuptools-scm

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import find_packages, setup
 
-import roadie
-
 setup(
     packages=find_packages(),
-    version=roadie.infer_version("EFAAR-benchmarking"),
     include_package_data=True,
 )
