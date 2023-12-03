d0901f1ebb929fb2d30935b4b97c26abdeaaf05a
Safiye Celik <safiye.celik@recursionpharma.com>
2023-12-02 23:08:32 -0500

    add random line to plotting function

--- efaar_benchmarking/plotting.py
+++ efaar_benchmarking/plotting.py
@@ -5,7 +5,7 @@ import pandas as pd
 import seaborn as sns
 
 
-def plot_recall(df):
+def plot_recall(df, color="red"):
     """
     Plots a line plot of recall values for several thereshold pairs for each source.
 
@@ -15,7 +15,9 @@ def plot_recall(df):
     Returns:
     None
     """
-    xy_pairs = [col.split("_")[1:] for col in df.columns if col.startswith("recall_")]
+    recall_thr_pairs = [col.split("_")[1:] for col in df.columns if col.startswith("recall_")]
+    x_values = [f"{x}, {y}" for x, y in recall_thr_pairs]
+    random_recall_values = [float(x) + 1 - float(y) for x, y in recall_thr_pairs]
 
     col_cnt = 5
     sns.set_style("whitegrid")
@@ -23,20 +25,22 @@ def plot_recall(df):
         nrows=math.ceil(len(df["source"].unique()) / col_cnt), ncols=col_cnt, figsize=(15, 3), squeeze=False
     )
 
-    color = "r"
     # Plot each source as a separate subplot
     for i, source in enumerate(df["source"].unique()):
         source_df = df[df["source"] == source]
-        x_values_orig = [f"{x}, {y}" for x, y in xy_pairs]
-        y_values = [list(source_df[f"recall_{x}_{y}"]) for x, y in xy_pairs]
-        x_values = [i for i, sublist in zip(x_values_orig, y_values) for _ in sublist]
-        tmp_data = pd.DataFrame({"x": x_values, "y": sum(y_values, [])})
+        y_values = [list(source_df[f"recall_{x}_{y}"]) for x, y in recall_thr_pairs]
+        # repeat x_values to match the length of y_values since we have multiple iterations of computations
+        # if x_values=["0.01, 0.99", "0.05, 0.95", "0.1, 0.9"] and there are three iterations of computations,
+        # then x_values_all=["0.01, 0.99", "0.01, 0.99", "0.01, 0.99", "0.05, 0.95", "0.05, 0.95", "0.05, 0.95",
+        # "0.1, 0.9", "0.1, 0.9", "0.1, 0.9"]
+        x_values_all = [i for i, sublist in zip(x_values, y_values) for _ in sublist]
+        recall_data = pd.DataFrame({"x": x_values_all, "y": sum(y_values, [])})
         curr_ax = axs[i // col_cnt, i % col_cnt]
         sns.lineplot(
             ax=curr_ax,
             x="x",
             y="y",
-            data=tmp_data,
+            data=recall_data,
             color=color,
             marker="o",
             markersize=8,
@@ -44,11 +48,25 @@ def plot_recall(df):
             markeredgewidth=2,
             errorbar=("ci", 95),
         )
+        random_values = [i for i, sublist in zip(random_recall_values, y_values) for _ in sublist]
+        random_data = pd.DataFrame({"x": x_values_all, "y": random_values})
+        sns.lineplot(
+            ax=curr_ax,
+            x="x",
+            y="y",
+            data=random_data,
+            color="gray",
+            marker="o",
+            markersize=5,
+            markerfacecolor="gray",
+            markeredgewidth=2,
+            errorbar=("ci", 95),
+        )
         curr_ax.set_title(source)
         curr_ax.set_xlabel("Recall Thresholds")
         curr_ax.set_ylabel("Recall Value")
-        curr_ax.set_xticks(range(len(x_values_orig)))
-        curr_ax.set_xticklabels(x_values_orig, rotation=45)
+        curr_ax.set_xticks(range(len(x_values)))
+        curr_ax.set_xticklabels(x_values, rotation=45)
 
     plt.tight_layout()
     plt.show()
