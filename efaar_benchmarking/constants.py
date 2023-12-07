169cd3808fcaf12810467fc9bb90d3abb384cffb
Safiye Celik <safiye.celik@recursionpharma.com>
2023-12-07 00:29:05 -0500

    plotting

--- efaar_benchmarking/constants.py
+++ efaar_benchmarking/constants.py
@@ -1,7 +1,7 @@
 from importlib import resources
 
 BENCHMARK_DATA_DIR = str(resources.files("efaar_benchmarking").joinpath("benchmark_annotations"))
-BENCHMARK_SOURCES = ["Reactome", "HuMAP", "CORUM", "SIGNOR", "StringDB"]
+BENCHMARK_SOURCES = ["CORUM", "HuMAP", "Reactome", "SIGNOR", "StringDB"]
 RECALL_PERC_THRS = [(0.05, 0.95), (0.1, 0.9)]
 RANDOM_SEED = 42
 RANDOM_COUNT = 3
