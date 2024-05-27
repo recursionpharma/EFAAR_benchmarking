e10eaf373f5f8d34fe688263e0719c9a2a35ff89
Safiye Celik <safiye.celik@recursionpharma.com>
2024-05-27 15:20:14 -0400

    update dependencies

--- efaar_benchmarking/efaar.py
+++ efaar_benchmarking/efaar.py
@@ -3,7 +3,7 @@ from typing import Optional
 import numpy as np
 import pandas as pd
 import scanpy as sc
-import scipy.linalg as linalg
+from scipy import linalg
 from scvi.model import SCVI
 from sklearn.covariance import EllipticEnvelope
 from sklearn.decomposition import PCA
