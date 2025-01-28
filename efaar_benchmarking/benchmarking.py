60df3eb267de3ba13b95f720b2a68c85f6b63d14
Oren Kraus <9474407+okraus@users.noreply.github.com>
2025-01-27 21:48:25 -0500

    added filter for truth annotations to support CellProfiler benchmarking (#95)
    * added filter for truth annotations to support CellProfiler benchmarking
    
    * removed print bc it pollutes logs

--- efaar_benchmarking/benchmarking.py
+++ efaar_benchmarking/benchmarking.py
@@ -911,6 +911,10 @@ def compound_gene_benchmark(
     """
     config = config or BenchmarkConfig()
     truth = truth_data if truth_data is not None else load_truth_data(benchmark_data_dir)
+    # filter truth relationships for genes & compounds in [filtered] metadata
+    truth = truth[
+        truth.gene_symbol.isin(map_data.metadata.perturbation) & truth.treatment.isin(map_data.metadata.perturbation)
+    ]
     similarities = compute_similarities(truth, map_data, pert_col, randomize=check_random)
 
     thresholds = (activity_threshold, inactivity_threshold)
